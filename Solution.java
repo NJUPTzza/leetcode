f1e59b272c5077a325f187fac1426edc6069b16e
zza <b23040218@njupt.edu.cn>
2025-01-03 14:48:10 +0800

    3

--- Solution.java
+++ Solution.java
@@ -1,26 +1,3 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        int cur = 0, maxLen = 0, ans = 0;
-        for (int num : nums) {
-            set.add(num);
-        }
-        for (int i : set) {
-            if (!set.contains(i - 1)) {
-                cur = i;
-                maxLen = 1;
-            }
-            while (set.contains(i + 1)) {
-                maxLen++;
-                i++;
-            }
-            ans = Math.max(maxLen, ans);
-        }
-        return ans;
-    }
+
 }
