38cb9932509d01c8120dc45e6a2e145c4ee4eebf
zza <921212318@qq.com>
2025-02-04 23:18:03 +0800

    5

--- Solution.java
+++ Solution.java
@@ -1,44 +1,19 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
+    public int subarraySum(int[] nums, int k) {
         int n = nums.length;
-        List<List<Integer>> ans = new ArrayList<>();
-        // 枚举 i
+        int pre = 0; 
+        int ans = 0;
+        Map<Integer, Integer> mp = new HashMap<>();
+        mp.put(0, 1);
         for (int i = 0; i < n; i++) {
-            // 如果 i 与上次重复，就直接省略，减少重复
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
+            pre += nums[i];
+            if (mp.containsKey(pre - k)) {
+                ans += mp.get(pre - k);
             }
-            int k = n - 1;
-            // 利用 j + k = -i 来寻找符合条件的 j 和 k
-            int target = -nums[i];
-            // 枚举 j
-            for (int j = i + 1; j < n; j++) {
-                // 如果 j 与上次重复，就直接省略，减少重复
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
-                    continue;
-                }
-                // 保证 j 在 k 左边的同时不断将 k 左移，枚举 k
-                while (j < k && nums[j] + nums[k] > target) {
-                    k--;
-                }
-                // 如果 j 与 k 重叠，则说明这个 i 情况已经枚举完毕
-                if (j == k) {
-                    break;
-                }
-                // 如果等于，则加入ans中
-                if (nums[j] + nums[k] == target) {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[k]);
-                    ans.add(list);
-                }
-            }
+            mp.put(pre, mp.getOrDefault(pre, 0) + 1);
         }
         return ans;
     }
